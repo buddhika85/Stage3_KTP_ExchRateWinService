481983815eb8102faff33fa34bca128c61a3b23a
buddhika85 <buddhika2006system@gmail.com>
2015-11-30 14:57:49 +0000

    Made changes - Preivouse external web service is not working, so changed to consume another new web service to get the euro and usd exchange rates

--- Exchange Rate Windows Service/ExchangeRateWinService/ExternalServiceConsumer.cs
+++ Exchange Rate Windows Service/ExchangeRateWinService/ExternalServiceConsumer.cs
@@ -19,18 +19,40 @@ namespace ExchangeRateWinService
         /// </summary>
         internal static decimal? GetEuroExchangeRateValue()
         {
-            string xmlResult = null;
+            //string xmlResult = null;
+            //decimal? euroExchange = null;
+            //string url = CSVReader.ConfigCsv.EuroUrl;
+            //try
+            //{
+            //    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            //    HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            //    StreamReader resStream = new StreamReader(response.GetResponseStream());
+            //    XmlDocument doc = new XmlDocument();
+            //    xmlResult = resStream.ReadToEnd();
+            //    doc.LoadXml(xmlResult);
+            //    euroExchange = Convert.ToDecimal(doc.GetElementsByTagName("double").Item(0).InnerText);
+            //    Logger.LogInfo("Euro exchange rate received");
+            //}
+            //catch (Exception exce)
+            //{
+            //    Logger.LogExceptions("Exception - Euro exchange rate consumption issue", exce);
+            //    throw exce;
+            //}
+            //return euroExchange;
+
+            string result = null;
             decimal? euroExchange = null;
-            string url = CSVReader.ConfigCsv.EuroUrl;
+            // old - do not work - http://www.webservicex.net/CurrencyConvertor.asmx/ConversionRate?FromCurrency=GBP&ToCurrency=USD
+            // new - works - "http://currencies.apps.grandtrunk.net/getlatest/gbp/usd";  // http://currencies.apps.grandtrunk.net/getlatest/gbp/eur 
+            string url = CSVReader.ConfigCsv.UsdUrl;
             try
             {
                 HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
                 HttpWebResponse response = (HttpWebResponse)request.GetResponse();
                 StreamReader resStream = new StreamReader(response.GetResponseStream());
                 XmlDocument doc = new XmlDocument();
-                xmlResult = resStream.ReadToEnd();
-                doc.LoadXml(xmlResult);
-                euroExchange = Convert.ToDecimal(doc.GetElementsByTagName("double").Item(0).InnerText);
+                result = resStream.ReadToEnd();
+                euroExchange = Convert.ToDecimal(result);
                 Logger.LogInfo("Euro exchange rate received");
             }
             catch (Exception exce)
@@ -46,23 +68,45 @@ namespace ExchangeRateWinService
         /// </summary>
         internal static decimal? GetUSDExchangeRateValue()
         {
-            string xmlResult = null;
+            //string xmlResult = null;
+            //decimal? usdExchange = null;
+            //string url = CSVReader.ConfigCsv.UsdUrl;
+            //try
+            //{
+            //    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            //    HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            //    StreamReader resStream = new StreamReader(response.GetResponseStream());
+            //    XmlDocument doc = new XmlDocument();
+            //    xmlResult = resStream.ReadToEnd();
+            //    doc.LoadXml(xmlResult);
+            //    usdExchange = Convert.ToDecimal(doc.GetElementsByTagName("double").Item(0).InnerText);
+            //    Logger.LogInfo("USD exchange rate received");
+            //}
+            //catch (Exception exce)
+            //{
+            //    Logger.LogExceptions("Exception - USD exchange rate consumption issue", exce);
+            //    throw exce;
+            //}
+            //return usdExchange;
+
+            string result = null;
             decimal? usdExchange = null;
-            string url = CSVReader.ConfigCsv.UsdUrl;
+            // old - do not work - http://www.webservicex.net/CurrencyConvertor.asmx/ConversionRate?FromCurrency=GBP&ToCurrency=USD
+            // new - works - "http://currencies.apps.grandtrunk.net/getlatest/gbp/usd";  // http://currencies.apps.grandtrunk.net/getlatest/gbp/eur 
+            string url = CSVReader.ConfigCsv.UsdUrl; 
             try
             {
                 HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
                 HttpWebResponse response = (HttpWebResponse)request.GetResponse();
                 StreamReader resStream = new StreamReader(response.GetResponseStream());
                 XmlDocument doc = new XmlDocument();
-                xmlResult = resStream.ReadToEnd();
-                doc.LoadXml(xmlResult);
-                usdExchange = Convert.ToDecimal(doc.GetElementsByTagName("double").Item(0).InnerText);
+                result = resStream.ReadToEnd();
+                usdExchange = Convert.ToDecimal(result);
                 Logger.LogInfo("USD exchange rate received");
             }
             catch (Exception exce)
             {
-                Logger.LogExceptions("Exception - Euro exchange rate consumption issue", exce);
+                Logger.LogExceptions("Exception - USD exchange rate consumption issue", exce);
                 throw exce;
             }
             return usdExchange;

f2d3494ccd68d3b06e148d69b6350e3853b130a9
buddhika85 <buddhika2006system@gmail.com>
2015-12-02 16:57:45 +0000

    Emailing when service started, stopped, unsual value consumed from external web service...etc

--- Exchange Rate Windows Service/ExchangeRateWinService/ExternalServiceConsumer.cs
+++ Exchange Rate Windows Service/ExchangeRateWinService/ExternalServiceConsumer.cs
@@ -50,13 +50,21 @@ namespace ExchangeRateWinService
                 HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
                 HttpWebResponse response = (HttpWebResponse)request.GetResponse();
                 StreamReader resStream = new StreamReader(response.GetResponseStream());
-                XmlDocument doc = new XmlDocument();               
+                result = resStream.ReadToEnd();              
                 euroExchange = Convert.ToDecimal(result);
-                Logger.LogInfo("Euro exchange rate received");
+                if (euroExchange == null || euroExchange.ToString() == string.Empty || euroExchange.ToString() == "0.0" || euroExchange.ToString() == "0")
+                {
+                    Logger.LogWarn(string.Format("Unusual euro exchange rate value {0} at {1}", euroExchange, DateTime.Now));
+                    new Emailer().InformViaEmail("BCMY Exchange Rate Windows Service - Unusual euro value", string.Format("Unusual euro exchange rate value {0} at {1}", euroExchange, DateTime.Now), null, null);
+                }
+                else {
+                    Logger.LogInfo("Euro exchange rate received");
+                }                
             }
             catch (Exception exce)
             {
                 Logger.LogExceptions("Exception - Euro exchange rate consumption issue", exce);
+                new Emailer().InformViaEmail("BCMY Exchange Rate Windows Service - Error", string.Format("Error occured when cosuming euro exchange rate at {0}", DateTime.Now), exce.Source, exce.InnerException.Message);
                 throw exce;
             }
             return euroExchange;
@@ -100,11 +108,20 @@ namespace ExchangeRateWinService
                 StreamReader resStream = new StreamReader(response.GetResponseStream());                
                 result = resStream.ReadToEnd();
                 usdExchange = Convert.ToDecimal(result);
-                Logger.LogInfo("USD exchange rate received");
+                if (usdExchange == null || usdExchange.ToString() == string.Empty || usdExchange.ToString() == "0.0" || usdExchange.ToString() == "0")
+                {
+                    Logger.LogWarn(string.Format("Unusual usd exchange rate value {0} at {1}", usdExchange, DateTime.Now));
+                    new Emailer().InformViaEmail("BCMY Exchange Rate Windows Service - Unusual usd value", string.Format("Unusual usd exchange rate value {0} at {1}", usdExchange, DateTime.Now), null, null);
+                }
+                else
+                {
+                    Logger.LogInfo("USD exchange rate received");
+                }               
             }
             catch (Exception exce)
             {
                 Logger.LogExceptions("Exception - USD exchange rate consumption issue", exce);
+                new Emailer().InformViaEmail("BCMY Exchange Rate Windows Service - Error", string.Format("Error occured when cosuming euro exchange rate at {0}", DateTime.Now), exce.Source, exce.InnerException.Message);
                 throw exce;
             }
             return usdExchange;
